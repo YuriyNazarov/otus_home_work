4738599e95abb865fa1c233c5a6aa5867846e140
yur.nazarov <yur.nazarov@x5.ru>
2022-05-04 16:00:15 +0300

    homework 6 try 1

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,46 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("abort after one", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		// Abort after 500 (5 stages * 100 ms)
+		abortDur := sleepPerStage * 5
+		go func() {
+			<-time.After(abortDur)
+			close(done)
+		}()
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 1)
+		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
+	})
+
+	t.Run("edge case", func(t *testing.T) {
+		in := make(Bi)
+		close(in)
+		result := make([]string, 0, 10)
+		var emptyStages []Stage
+		for s := range ExecutePipeline(in, nil, emptyStages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Len(t, result, 0)
+	})
 }
